571584f8a2c3654e64fdaa9a65b21df57d0be272
Tara Nelson & Spyder Monkee McHenry <student@epicodus.com>
2014-04-09 09:01:20 -0700

    project set up-  ready for lift off...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhotoSharing::Application.config.secret_key_base = 'e89d56f4285595003a1e942b8ade49e0b006d48e9b79dcf3afc3ba47a888380c0a286560c11730f505f8098cdeea425012cbd27390e0fbd34ea631dbe95324eb'
