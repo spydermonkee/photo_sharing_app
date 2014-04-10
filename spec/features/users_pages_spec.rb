dd8b5dbfdcf9b517e1a6b0b09b9f0b93486b1573
Spyder Monkee McHenry & David Torbeck <student@epicodus.com>
2014-04-10 11:22:38 -0700

    adds capybara tests for logout form both user show and user home page

--- spec/features/users_pages_spec.rb
+++ spec/features/users_pages_spec.rb
@@ -17,7 +17,7 @@ describe User do
   end
 
   context 'sign in' do
-    it 'lets a user log in' do
+    it 'lets a user login' do
       user = FactoryGirl.create :user
       visit root_path
       click_link 'Login'
@@ -25,6 +25,33 @@ describe User do
       fill_in 'Password', with: user.password
       click_button 'Login'
       page.should have_content "Logged in as #{user.name}"
+    end
+  end
+
+  context 'logout' do
+    it 'lets a user logout from the user show page' do
+      user = FactoryGirl.create :user
+      visit root_path
+      click_link 'Login'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Login'
+      click_link 'Log the F*^$ out!'
+      page.should have_content "Logged out"
+    end
+  end
+
+  context 'logout' do
+    it 'lets a user logout from the root page' do
+      user = FactoryGirl.create :user
+      visit root_path
+      click_link 'Login'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Login'
+      visit root_path
+      click_link 'Log the F*^$ out!'
+      page.should have_content "Logged out"
     end
   end
 end
