ef76ebde72c04fa9c26c1436ade574c0123acbbf
Spyder Monkee McHenry & David Torbeck <student@epicodus.com>
2014-04-10 10:09:05 -0700

    adds basic rspec for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
 
   validates :email, :uniqueness => true, :presence => true
   validates :name, :presence => true
+  validates :password_digest, :presence => true
 
 
 end
